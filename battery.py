b0bef083afd9339bc6d4374a1c812d43db9afb62
amitiwary999 <tiwarya9@gmail.com>
2017-02-20 04:01:29 +0530

    Fixed #1 added keyboard interrupt handling

--- battery.py
+++ battery.py
@@ -3,25 +3,28 @@ import time
 from gi.repository import Notify
 import commands
 def battery():
-	state=commands.getoutput('upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state"')
-	print state
+        try:
+                state=commands.getoutput('upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state"')
+                print state
 
-	# Run till battery is fully charged
-	full=0
-	while(full==0):
-		state=commands.getoutput('upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state"')
-		print commands.getoutput('clear')
-		print state
-		if(state=='    state:               fully-charged'):
-			full=1
-	print "FULLY CHARGED"
+                # Run till battery is fully charged
+                full=0
+                while(full==0):
+                        state=commands.getoutput('upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state"')
+                        print commands.getoutput('clear')
+                        print state
+                        if(state=='    state:               fully-charged'):
+                                full=1
+                print "FULLY CHARGED"
 
-	# Reminding every minute when battery is fully charged
-	while state=='    state:               fully-charged':
-		Notify.init("BAT FULL")
-		st=Notify.Notification.new("Battery","fully-charged","dialog-information")
-		st.show()
-		time.sleep(60)
-		state=commands.getoutput('upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state"')
-	# battery() # call itself when the charge is disconnected
+                # Reminding every minute when battery is fully charged
+                while state=='    state:               fully-charged':
+                        Notify.init("BAT FULL")
+                        st=Notify.Notification.new("Battery","fully-charged","dialog-information")
+                        st.show()
+                        time.sleep(60)
+                        state=commands.getoutput('upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state"')
+                # battery() # call itself when the charge is disconnected
+        except:
+                print "interrupted..Exiting"
 battery()
